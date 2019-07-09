b455de9e191f47c23ed65f56f85d12cc86efa708
Benjamin Beggs <benbeggs@ciena.com>
2019-07-09 13:05:48 -0400

    Update EnhancedOldBuildDiscarderTest.java

--- src/test/java/org/jenkinsci/plugins/enhancedoldbuilddiscarder/EnhancedOldBuildDiscarderTest.java
+++ src/test/java/org/jenkinsci/plugins/enhancedoldbuilddiscarder/EnhancedOldBuildDiscarderTest.java
@@ -4,7 +4,6 @@
 package org.jenkinsci.plugins.enhancedoldbuilddiscarder;
 
 import hudson.model.*;
-import hudson.util.RunList;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,48 +26,69 @@ public class EnhancedOldBuildDiscarderTest {
 	@Test
 	public void holdMaxBuilds() throws Exception {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyymmdd");
-		FreeStyleProject project = jRule.createFreeStyleProject("test");
-		FreeStyleBuild build1 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build3 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build4 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build5 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build6 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build7 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build8 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build9 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build10 = project.scheduleBuild2(0).get();
-		jRule.assertBuildStatus(Result.SUCCESS, build1);
-		jRule.assertBuildStatus(Result.SUCCESS, build2);
-		jRule.assertBuildStatus(Result.SUCCESS, build3);
-		jRule.assertBuildStatus(Result.SUCCESS, build4);
-		jRule.assertBuildStatus(Result.SUCCESS, build5);
-		jRule.assertBuildStatus(Result.SUCCESS, build6);
-		jRule.assertBuildStatus(Result.SUCCESS, build7);
-		jRule.assertBuildStatus(Result.SUCCESS, build8);
-		jRule.assertBuildStatus(Result.SUCCESS, build9);
-		jRule.assertBuildStatus(Result.SUCCESS, build10);
+		FreeStyleProject project = mock(FreeStyleProject.class);
+		project = jRule.createFreeStyleProject("test");
 		Calendar cal = Calendar.getInstance();
+
+		FreeStyleBuild build1 = mock(FreeStyleBuild.class);
+		build1 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120101"));
 		when(build1.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build1);
+
+		FreeStyleBuild build2 = mock(FreeStyleBuild.class);
+		build2 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120102"));
 		when(build2.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build2);
+
+		FreeStyleBuild build3 = mock(FreeStyleBuild.class);
+		build3 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120103"));
 		when(build3.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build3);
+
+		FreeStyleBuild build4 = mock(FreeStyleBuild.class);
+		build4 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120104"));
 		when(build4.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build4);
+
+		FreeStyleBuild build5 = mock(FreeStyleBuild.class);
+		build5 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120105"));
 		when(build5.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build5);
+
+		FreeStyleBuild build6 = mock(FreeStyleBuild.class);
+		build6 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120106"));
 		when(build6.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build6);
+
+		FreeStyleBuild build7 = mock(FreeStyleBuild.class);
+		build7 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120107"));
 		when(build7.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build7);
+
+		FreeStyleBuild build8 = mock(FreeStyleBuild.class);
+		build8 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120108"));
 		when(build8.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build8);
+
+		FreeStyleBuild build9 = mock(FreeStyleBuild.class);
+		build9 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120109"));
 		when(build9.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build9);
+
+		FreeStyleBuild build10 = mock(FreeStyleBuild.class);
+		build10 = spy(project.scheduleBuild2(0).get());
 		cal.setTime(sdf.parse("20120110"));
 		when(build10.getTimestamp()).thenReturn(cal);
+		jRule.assertBuildStatus(Result.SUCCESS, build10);
 
 		// are greater in amount than builds present, causing build discard queue to be cleared
 		EnhancedOldBuildDiscarder publisher = (new EnhancedOldBuildDiscarder(
@@ -76,10 +96,5 @@ public class EnhancedOldBuildDiscarderTest {
 				false, true));
 
 		publisher.perform(project);
-
-		//for (int i = 0; i < 10; i++) {
-		//	verify(buildListHMS.get(i), never()).delete();
-	//	}
-
 	}
 }
