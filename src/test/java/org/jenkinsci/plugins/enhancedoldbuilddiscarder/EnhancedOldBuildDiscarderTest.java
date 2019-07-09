8fba4b195adcf6c22073df785af72616524c7def
Benjamin Beggs <benbeggs@ciena.com>
2019-07-09 11:39:19 -0400

    Update EnhancedOldBuildDiscarderTest.java

--- src/test/java/org/jenkinsci/plugins/enhancedoldbuilddiscarder/EnhancedOldBuildDiscarderTest.java
+++ src/test/java/org/jenkinsci/plugins/enhancedoldbuilddiscarder/EnhancedOldBuildDiscarderTest.java
@@ -5,12 +5,9 @@ package org.jenkinsci.plugins.enhancedoldbuilddiscarder;
 
 import hudson.model.*;
 import hudson.util.RunList;
-import junit.framework.TestCase;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import static org.mockito.Mockito.*;
 import org.junit.Rule;
@@ -28,7 +25,8 @@ public class EnhancedOldBuildDiscarderTest {
 	public JenkinsRule jRule = new JenkinsRule();
 
 	@Test
-	public void successVsFailure() throws Exception {
+	public void holdMaxBuilds() throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyymmdd");
 		FreeStyleProject project = jRule.createFreeStyleProject("test");
 		FreeStyleBuild build1 = project.scheduleBuild2(0).get();
 		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
@@ -50,5 +48,38 @@ public class EnhancedOldBuildDiscarderTest {
 		jRule.assertBuildStatus(Result.SUCCESS, build8);
 		jRule.assertBuildStatus(Result.SUCCESS, build9);
 		jRule.assertBuildStatus(Result.SUCCESS, build10);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sdf.parse("20120101"));
+		when(build1.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120102"));
+		when(build2.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120103"));
+		when(build3.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120104"));
+		when(build4.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120105"));
+		when(build5.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120106"));
+		when(build6.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120107"));
+		when(build7.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120108"));
+		when(build8.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120109"));
+		when(build9.getTimestamp()).thenReturn(cal);
+		cal.setTime(sdf.parse("20120110"));
+		when(build10.getTimestamp()).thenReturn(cal);
+
+		// are greater in amount than builds present, causing build discard queue to be cleared
+		EnhancedOldBuildDiscarder publisher = (new EnhancedOldBuildDiscarder(
+				"10", "20", "", "",
+				false, true));
+
+		publisher.perform(project);
+
+		//for (int i = 0; i < 10; i++) {
+		//	verify(buildListHMS.get(i), never()).delete();
+	//	}
+
 	}
 }
